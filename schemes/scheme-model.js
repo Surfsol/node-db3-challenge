70aa5a741487f35e8f286910490ba4c75f67d296
Surfsol <surfsol@protonamil.com>
2019-11-07 09:26:03 -0300

    notes on model

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -11,7 +11,7 @@ module.exports = {
  }
 
 function find() {
-    return db('schemes')
+    return db('schemes')// means select all from table schemes
 }
 
 // `findById(id)`:
@@ -23,13 +23,12 @@ function find() {
 // where id = 1
 
 function findById(id) {
-    
+    //from db schemes where id = id
     return db('schemes')
     .where({id: id})
     
     
 }
-
 
 // select steps.id, schemes.scheme_name, steps.step_number, steps.instructions
 // from steps
@@ -45,12 +44,14 @@ function findSteps(id){
 function add(body){
     return db('schemes')
     .insert(body)
+    //resolves to an id of new user
 }
 
 function update(changes, id){
     return db('schemes')
     .update(changes)
     .where({id: id})
+    //resolve to count records updated
 }
 
 function remove(id){
